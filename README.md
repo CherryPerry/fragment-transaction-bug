9bc3d0b6dcba005c0b1e7fab4ba2a37412a8e766
CherryPerry <ya.cherryperry@yandex.ru>
2018-07-31 18:29:54 +0300

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+### Description
+
+If activity stops while fragment animated transaction,
+both of fragments are drawn,
+but old one is not in fragment stack, just drawn like a ghost.
+
+### Video
+
+![ezgif-2-cf08197654](https://user-images.githubusercontent.com/9081555/43469630-48f3759e-94ef-11e8-825b-5b18c92bf1c1.gif)
---
+++ app/src/main/java/ru/cherryperry/fragment/bug/MainActivity.java
@@ -0,0 +1,42 @@
+package ru.cherryperry.fragment.bug;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(android.R.id.content, new Fragment1())
+                    .commit();
+        }
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        } else {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(android.R.id.content, new Fragment2())
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+    }
+}
